c910152f600a4b5bad5a1359e84d598d4dd991d7
Marko Elezovic <marko.elezovic@oradian.com>
2022-04-23 00:12:23 +0200

    Add OxygenTrack

--- src/main/scala/mars/Main.scala
+++ src/main/scala/mars/Main.scala
@@ -6,27 +6,41 @@ enum ActionBonus {
   case TRBump
   case BonusOcean
   case BonusHeatProduction
+  case BonusTemperature
 }
 
-case class OceanTrack private(range: Range, current: Int) {
-  def advance(): IO[ActionError, (OceanTrack, Seq[ActionBonus])] =
-    if (current == range.last) {
+enum ActionError {
+  case OxygenMaxed
+  case TemperatureMaxed
+  case OceansMaxed
+}
+
+case class OxygenTrack private(current: Int) {
+  def advance(): IO[ActionError, (OxygenTrack, Seq[ActionBonus])] = {
+    if (current == OxygenTrack.Steps.last) {
       IO.fail(ActionError.OceansMaxed)
-    }  else {
-      IO.succeed(copy(current = current + range.step), Seq(ActionBonus.TRBump))
+    } else {
+      (current + OxygenTrack.Steps.step) match {
+        case no @ OxygenTrack.TemperatureSynergy =>
+          IO.succeed(copy(current = no), Seq(ActionBonus.TRBump, ActionBonus.BonusTemperature))
+        case nt =>
+          IO.succeed(copy(current = nt), Seq(ActionBonus.TRBump))
+      }
     }
+  }
 }
-object OceanTrack {
-  val Steps: Range = 0 to 9 by 1
-  val Start: OceanTrack = OceanTrack(Steps, Steps.head)
+object OxygenTrack {
+  private val Steps: Range = 0 to 13 by 1
+  private val TemperatureSynergy = 8
+  val Start: OxygenTrack = OxygenTrack(Steps.head)
 }
 
-case class TemperatureTrack private(range: Range, current: Int) {
+case class TemperatureTrack private(current: Int) {
   def advance(): IO[ActionError, (TemperatureTrack, Seq[ActionBonus])] = {
-    if (current == range.last) {
+    if (current == TemperatureTrack.Steps.last) {
       IO.fail(ActionError.TemperatureMaxed)
     } else {
-      (current + range.step) match {
+      (current + TemperatureTrack.Steps.step) match {
         case nt @ TemperatureTrack.OceanSynergy =>
           IO.succeed(copy(current = nt), Seq(ActionBonus.TRBump, ActionBonus.BonusOcean))
         case nt if TemperatureTrack.HeatProductionSynergy(nt) =>
@@ -38,40 +52,50 @@ case class TemperatureTrack private(range: Range, current: Int) {
   }
 }
 object TemperatureTrack {
-  val Steps: Range = -30 to 8 by 2
-  val OceanSynergy = 0
-  val HeatProductionSynergy = Set(-24, -20)
-  val Start: TemperatureTrack = TemperatureTrack(Steps, Steps.head)
+  private val Steps: Range = -30 to 8 by 2
+  private val OceanSynergy = 0
+  private val HeatProductionSynergy = Set(-24, -20)
+  val Start: TemperatureTrack = TemperatureTrack(Steps.head)
 }
 
-enum ActionError {
-  case OceansMaxed
-  case TemperatureMaxed
-  case TileAlreadyPlaced
+case class OceanTrack private(current: Int) {
+  def advance(): IO[ActionError, (OceanTrack, Seq[ActionBonus])] =
+    if (current == OceanTrack.Steps.last) {
+      IO.fail(ActionError.OceansMaxed)
+    }  else {
+      IO.succeed(copy(current = current + OceanTrack.Steps.step), Seq(ActionBonus.TRBump))
+    }
+}
+object OceanTrack {
+  private val Steps: Range = 0 to 9 by 1
+  val Start: OceanTrack = OceanTrack(Steps.head)
 }
 
 case class GlobalParameters private(
+  oxygen: OxygenTrack,
   temperature: TemperatureTrack,
   oceans: OceanTrack,
 ) {
   def apply(action: UIO[Action]): IO[ActionError, (GlobalParameters, Seq[ActionBonus])] =
     action flatMap {
-      case Action.PlaceOcean(tile) =>
-        oceans.advance() map { case (ot, sab) =>
-          (copy(oceans = ot), sab)
+      case Action.IncreaseOxygen =>
+        oxygen.advance() map { case (ot, sab) =>
+          (copy(oxygen = ot), sab)
         }
       case Action.IncreaseTemperature =>
         temperature.advance() map { case (tt, sab) =>
           (copy(temperature = tt), sab)
         }
-      case _ => ???
+      case Action.PlaceOcean(tile) =>
+        oceans.advance() map { case (ot, sab) =>
+          (copy(oceans = ot), sab)
+        }
     }
 }
 
 object GlobalParameters {
-  val TemperatureSteps: Range = -30 to 8 by 2
-
   val Start: GlobalParameters = GlobalParameters(
+    oxygen = OxygenTrack.Start,
     temperature = TemperatureTrack.Start,
     oceans = OceanTrack.Start,
   )
@@ -86,22 +110,11 @@ object Action {
 
 case class Tile(row: Int, column: Int)
 case class Row(tiles: IndexedSeq[Tile])
-case class Board(row: Int, rows: IndexedSeq[Row])
+case class Board(rows: IndexedSeq[Row])
 
 object Board {
   def apply(): Board = {
-    """
-         SS [SS] .. [C.][..]
-       ..  S.  ..  ..  .. [CC]
-     C.  ..  ..  ..  ..  ..  S.
-   PT  P.  P.  P.  PP  P.  P. [PP]
- PP  PP  PP [PP][PP][PP] PP  PP  PP
-   P.  PP  P.  P.  P. [P.][P.][P.]
-     ..  ..  ..  ..  ..  P.  ..
-       SS  ..  C.  C.  ..  T.
-         S.  SS  ..  .. [TT]
-"""
-    ???
+    new Board(IndexedSeq.empty)
   }
 }
 
@@ -114,4 +127,3 @@ object Main extends App {
   val state = runtime.unsafeRun(x)
   println(state)
 }
-
